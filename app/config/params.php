0ba40804bb7387dd7498d245e0143463a09aae0d
youssef hatoufi <hatoufiyoussef@gmail.com>
2025-10-06 16:31:06 +0200

    structure mvc initiale

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// params
+
+// paramètre de connexion à la db
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'alex_parker_octobre_2021');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
---
+++ core/init.php
@@ -0,0 +1,8 @@
+<?php
+// Initialisation
+
+//1. charger les params
+require_once '../app/config/params.php';
+
+//2. charger la connexion
+require_once '../core/connexion.php';
